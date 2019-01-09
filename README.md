1b9578d988614e91aac9d3e7ed5ed44c91932c69
OtsukaNoboru <mopinfish@gmail.com>
2019-01-10 01:33:16 +0900

    [gas] daily_tasks | tasks from spreadsheet

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # gas
 GoogleAppScript
+
+## commands
+
+### clone project
+```
+mkdir project_name && cd project_name
+clasp clone GAS_ID
+```
--- daily_tasks/main.js
+++ daily_tasks/main.js
@@ -1,27 +1,54 @@
 function main() {
-  addTasks();
+  addRegularTasks();
 }
 
 /**
  * タスクの追加
+ *
+ * @param {string} body タスク内容
+ * @param {number} room_id ルームID
+ * @param {number} 期限（UNIXTIME）
+ * @param {Array.<number>} account_ids 担当者のID（数値の配列）
  */
-function addTasks() {
+function addTask(body, room_id, limit, account_ids) {
  
   const token = UserProperties.getProperty('CHATWORK_TOKEN') // チャットワークAPIトークン
-      , id = UserProperties.getProperty('CHATWORK_ID') // チャットワークID
-      , room_id = PropertiesService.getScriptProperties().getProperty('ROOM_ID') // ルームID
-      , date = new Date() // 今日の日付
-      , limit = date.getTime()/1000 // 秒単位のUNIX時間を取得 
       , client = ChatWorkClient.factory({token: token}) // チャットワーククライアント
       ;
 
   const params = {
-    body: "ミルクを買う", // タスクの内容
+    body: body, // タスクの内容
     room_id: room_id,
     limit: limit, // タスクの期限
-    to_id_list: [id] // アカウントIDのリスト
+    to_id_list: account_ids // アカウントIDのリスト
   };
 
   client.sendTask(params);
 
+}
+
+function addRegularTasks() {
+  const spreadsheetId = '18Ze4XnW-080QN5_fK7-jBacWj-RgqVXcctWYsryAfOQ'
+      , taskListSheet = SpreadsheetApp.openById(spreadsheetId)
+      , taskCount = taskListSheet.getDataRange().getLastRow() - 1 // 見出し行分を引いておく
+      , taskData = taskListSheet.getDataRange().getValues()
+      , date = new Date() // 今日の日付
+      , limit = (date.getTime() / 1000).toFixed() // 秒単位のUNIX時間を取得 
+      ;
+
+  // 見出し行を削除
+  taskData.shift();
+
+  for(var i = 0; i < taskCount; i++) {
+    var row = taskData[i];
+
+    if (row[4-1] === "毎日") {
+      var body = row[1-1]         // タスク内容
+        , room_id = (row[2-1]).toString(10)      // ルームID
+        , account_ids = (row[3-1]).toString(10).split(',')  // アカウントID
+        ;
+
+      addTask(body, room_id, limit, account_ids);
+    }
+  }
 }
