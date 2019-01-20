2a19ecca282c9a5278583f27fa13b56e62f1c356
OtsukaNoboru <mopinfish@gmail.com>
2019-01-20 13:56:14 +0900

    [gas]daily tasks | tasks filtered by dateNum

--- daily_tasks/main.js
+++ daily_tasks/main.js
@@ -27,6 +27,9 @@ function addTask(body, room_id, limit, account_ids) {
 
 }
 
+/**
+ * スプレッドシートから取得したタスクをチャットワークに登録する
+ */
 function addRegularTasks() {
   const spreadsheetId = '18Ze4XnW-080QN5_fK7-jBacWj-RgqVXcctWYsryAfOQ'
       , taskListSheet = SpreadsheetApp.openById(spreadsheetId)
@@ -34,21 +37,22 @@ function addRegularTasks() {
       , taskData = taskListSheet.getDataRange().getValues()
       , date = new Date() // 今日の日付
       , limit = (date.getTime() / 1000).toFixed() // 秒単位のUNIX時間を取得 
+      , todayNum = Math.floor(date.getDay()).toString(10)
       ;
 
   // 見出し行を削除
   taskData.shift();
 
-  for(var i = 0; i < taskCount; i++) {
-    var row = taskData[i];
+  taskData.forEach(function (row, index) {
+    const body = row[1-1]         // タスク内容
+      , room_id = (row[2-1]).toString(10)      // ルームID
+      , account_ids = (row[3-1]).toString(10).split(',')  // アカウントID
+      , dayList = row[4-1].split(',')
+      ;
 
-    if (row[4-1] === "毎日") {
-      var body = row[1-1]         // タスク内容
-        , room_id = (row[2-1]).toString(10)      // ルームID
-        , account_ids = (row[3-1]).toString(10).split(',')  // アカウントID
-        ;
-
+    if (dayList.indexOf(todayNum) >= 0) {
       addTask(body, room_id, limit, account_ids);
     }
-  }
+
+  });
 }
