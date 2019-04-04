586daab8f103424b479f4b21477414b44e95e803
OtsukaNoboru <mopinfish@gmail.com>
2019-04-04 22:01:33 +0900

    [gas] feedly_notify | refactor indent2

--- feedly_notify/src/services/chatwork.ts
+++ feedly_notify/src/services/chatwork.ts
@@ -8,18 +8,18 @@ const ChatworkService = {
    * @param {string} body メッセージ内容
    */
   sendMessage: (body) => {
-   
-  	const properties = PropertiesService.getScriptProperties()
-  		 	, token = properties.getProperty('CHATWORK_TOKEN') // Chatwork APIトークン
-  			, roomId = properties.getProperty('CHATWORK_ROOM_ID') // Chatwork ルームID
-  		 	, client = ChatWorkClient.factory({token: token}) // チャットワーククライアント
-  		 	;
-  
+
+    const properties = PropertiesService.getScriptProperties()
+      , token = properties.getProperty('CHATWORK_TOKEN') // Chatwork APIトークン
+      , roomId = properties.getProperty('CHATWORK_ROOM_ID') // Chatwork ルームID
+      , client = ChatWorkClient.factory({token: token}) // チャットワーククライアント
+    ;
+
     const params = {
       room_id: roomId,
       body: body, // タスクの内容
     };
-  
+
     client.sendMessage(params);
   }
 };
