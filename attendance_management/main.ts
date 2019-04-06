869621809b22015c6aaa36783dc678fe18eee659
OtsukaNoboru <mopinfish@gmail.com>
2019-04-06 18:54:24 +0900

    [gas] attendance_management | add spreadsheet manipulation

--- attendance_management/main.ts
+++ attendance_management/main.ts
@@ -1,10 +1,23 @@
-function myFunction() {
-  Logger.log('hogehoge');
-}
+/**
+ * main method for GET REQUEST
+ * @return {Object} HtmlService Object
+ */
 function doGet() {
   var template = 'index';
   return HtmlService.createTemplateFromFile(template).evaluate();
 }
+
 function include(filename) {
   return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
+
+// クライアント一覧を取得
+function getSheetDataCliant() {
+  const properties = PropertiesService.getScriptProperties()
+      ;
+  var spreadSheetID = properties.getProperty('SHEET_ID');
+  var sheetName = "master";
+  var res = SpreadsheetApp.openById(spreadSheetID).getSheetByName(sheetName).getDataRange().getValues();
+  res = Array.prototype.concat.apply([], res);
+  return res;
 }
